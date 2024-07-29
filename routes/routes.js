fe71705b24ae598f20045571c585ddf427a6fcda
TiagoSolinf <167105119+TiagoSolinf@users.noreply.github.com>
2024-07-29 14:06:31 +0100

    route fixed

--- routes/routes.js
+++ routes/routes.js
@@ -8,6 +8,6 @@ router.post('/email/warranty', EmailController.sendGarantiaEmail);
 router.post('/email/repair', EmailController.sendReparacaoEmail);
 router.post('/email/general', EmailController.sendGeneralEmail)
 router.post('/email/questions', EmailController.sendQuestionsEmail)
-router.post('/sendOptionBasedEmail', EmailController.sendOptionBasedEmail);
+router.post('/email/sendOptionBasedEmail', EmailController.sendOptionBasedEmail);
 
 module.exports = router;
