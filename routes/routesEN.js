f945b74f43ea9a42ee00c82c167dc53e2174bdd0
TiagoSolinf <167105119+TiagoSolinf@users.noreply.github.com>
2024-08-02 10:59:10 +0100

    routes fix

--- routes/routesEN.js
+++ routes/routesEN.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post('/en/email/return', EmailController.sendReturnEmailEN);
 router.post('/en/email/warranty', EmailController.sendWarrantyEmailEN);
 router.post('/en/email/repair', EmailController.sendRepairEmailEN);
-router.post('/en/email/general', EmailController.sendGeneralEmailEN);
+
 router.post('/en/email/questions', EmailController.sendQuestionsEmailEN);
 router.post('en/email/sendOptionBasedEmail', EmailController.sendOptionBasedEmailEN);
 
