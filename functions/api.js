6f14e9272452b8cf185cf80271cd9f679d6ebcce
brunotrodrigues <brunotrodrigues16@outlook.com>
2024-08-02 14:39:47 +0100

    14:40 update rollback

--- functions/api.js
+++ functions/api.js
@@ -7,26 +7,22 @@ const emailRoutesEN = require('../routes/routesEN');
 require('dotenv').config();
 
 const app = express();
+const router = express.Router();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-    console.log("GET /");
+router.get('/', function (req, res) {
     res.status(200).json({ message: "Welcome to NodeMailer-API v1.0, by Solinf, Soluções Informáticas, Lda." });
 });
 
-app.use('/api', emailRoutes);
-app.use('/apiEN', emailRoutesEN);
+router.use('/api', emailRoutes);
+router.use('/api', emailRoutesEN);
 
-app.get('*', function (req, res) {
-    console.log("Route not found: ", req.originalUrl);
+router.get('*', function (req, res) {
     res.status(404).json({ message: "Error" });
 });
 
-app.use((err, req, res, next) => {
-    console.error("Internal Server Error: ", err.stack);
-    res.status(500).json({ message: "Internal Server Error" });
-});
+app.use('/.netlify/functions/api', router);
 
 module.exports.handler = serverless(app);
--- routes/routes.js
+++ routes/routes.js
@@ -1,19 +1,12 @@
 const express = require('express');
 const EmailController = require('../controllers/emailController');
+
 const router = express.Router();
 
 router.post('/email/return', EmailController.sendDevolucaoEmail);
 router.post('/email/warranty', EmailController.sendGarantiaEmail);
 router.post('/email/repair', EmailController.sendReparacaoEmail);
-router.post('/email/general', (req, res) => {
-    console.log("POST /api/email/general with data:", req.body);
-    try {
-        res.status(200).json({ message: 'Email sent!' });
-    } catch (error) {
-        console.error("Error handling /email/general:", error);
-        res.status(500).json({ message: "Internal Server Error" });
-    }
-});
-
+router.post('/email/questions', EmailController.sendQuestionsEmail);
+router.post('/email/general', EmailController.sendOptionBasedEmail);
 
 module.exports = router;
