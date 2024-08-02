fc4a1c0b423169857868b26b56402d96c8a1ac0e
brunotrodrigues <brunotrodrigues16@outlook.com>
2024-08-02 15:11:27 +0100

    rollback

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("This project is configured to use Netlify Functions for API endpoints.");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const emailRoutes = require('./routes/routes');
+const emailRoutesEN = require('./routes/routesEN');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+    res.status(200).json({message: "Welcome to NodeMailer-API v1.0, by Solinf, Soluções Informáticas, Lda."});
+})
+
+app.use('/api', emailRoutes);
+app.use('/api', emailRoutesEN);
+
+app.get('*', function (req, res) {
+    res.status(404).json({message: "Error"});
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
--- routes/routes.js
+++ routes/routes.js
@@ -7,9 +7,6 @@ router.post('/email/return', EmailController.sendDevolucaoEmail);
 router.post('/email/warranty', EmailController.sendGarantiaEmail);
 router.post('/email/repair', EmailController.sendReparacaoEmail);
 router.post('/email/questions', EmailController.sendQuestionsEmail);
-router.post('/email/general', (req, res, next) => {
-    console.log('Request body:', req.body);
-    next();
-}, EmailController.sendOptionBasedEmail);
+router.post('/email/general', EmailController.sendOptionBasedEmail);
 
 module.exports = router;
