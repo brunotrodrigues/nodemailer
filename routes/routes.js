a16ffddee6fe42fe77579ca9c4e14095869c00ef
brunotrodrigues <brunotrodrigues16@outlook.com>
2024-08-02 14:55:15 +0100

    small update

--- routes/routes.js
+++ routes/routes.js
@@ -7,6 +7,9 @@ router.post('/email/return', EmailController.sendDevolucaoEmail);
 router.post('/email/warranty', EmailController.sendGarantiaEmail);
 router.post('/email/repair', EmailController.sendReparacaoEmail);
 router.post('/email/questions', EmailController.sendQuestionsEmail);
-router.post('/email/general', EmailController.sendOptionBasedEmail);
+router.post('/email/general', (req, res, next) => {
+    console.log('Request body:', req.body);
+    next();
+}, EmailController.sendOptionBasedEmail);
 
 module.exports = router;
