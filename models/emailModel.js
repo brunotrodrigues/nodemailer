b23c48be70a75009ed072332716ea9f4376532b7
brunotrodrigues <brunotrodrigues16@outlook.com>
2024-08-05 11:25:39 +0100

    update

--- models/emailModel.js
+++ models/emailModel.js
@@ -12,7 +12,7 @@ class EmailModel {
         return nodemailer.createTransport({
             host: process.env.SMTP_SERVER,
             port: process.env.SMTP_PORT,
-            secure: false, // true for 465, false for other ports
+            secure: true, // true for 465, false for other ports
             auth: {
                 user: process.env.SMTP_USERNAME,
                 pass: process.env.SMTP_PASSWORD
