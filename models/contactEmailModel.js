b6bdb9fff6c2af4aacb2445a353f9f8d152509ca
brunotrodrigues <brunotrodrigues16@outlook.com>
2024-10-19 18:11:11 +0100

    changed vars in emails

--- models/contactEmailModel.js
+++ models/contactEmailModel.js
@@ -8,13 +8,13 @@ class ContactEmailModel {
             // faturacao: 'faturacao@sensordin.pt',
             // qualidade: 'qualidade@sensordin.pt',
             // logistica: 'logistica@sensordin.pt',
-            general: 'bruno.rodrigues@solinf.pt',
-            support: 'bruno.rodrigues@solinf.pt',
-            commercial: 'bruno.rodrigues@solinf.pt',
-            financial: 'bruno.rodrigues@solinf.pt',
-            billing: 'bruno.rodrigues@solinf.pt',
-            quality: 'bruno.rodrigues@solinf.pt',
-            logistics: 'bruno.rodrigues@solinf.pt',
+            general: 'brunotrodrigues16@outlook.com',
+            support: 'brunotrodrigues16@outlook.com',
+            commercial: 'brunotrodrigues16@outlook.com',
+            financial: 'brunotrodrigues16@outlook.com',
+            billing: 'brunotrodrigues16@outlook.com',
+            quality: 'brunotrodrigues16@outlook.com',
+            logistics: 'brunotrodrigues16@outlook.com',
         };
 
         return contactEmails[option];
